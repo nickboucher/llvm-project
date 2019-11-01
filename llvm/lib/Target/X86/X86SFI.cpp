2abf6c958affe8964f26ae88c8f07faf7a1bdf1f
Nicholas Boucher <nboucher@college.harvard.edu>
2019-11-01 15:40:55 -0700

    Example Machine Basic Block Iteration

--- llvm/lib/Target/X86/X86SFI.cpp
+++ llvm/lib/Target/X86/X86SFI.cpp
@@ -7,11 +7,43 @@
 
 
 #include "X86.h"
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "X86Subtarget.h"
 
 using namespace llvm;
 
 #define DEBUG_TYPE "x86-sfi"
 
+
+class X86SFIPass : public MachineFunctionPass {
+public:
+    X86SFIPass(): MachineFunctionPass(ID) {}
+    
+    bool runOnMachineFunction(MachineFunction &MF) override;
+    
+private:
+    StringRef getPassName() const override {
+        return "X86 SFI Pass";
+    }
+    static char ID;
+};
+
+char X86SFIPass::ID = 42;
+
+
 FunctionPass *llvm::createX86SFIPass() {
-  return nullptr;
+    return new X86SFIPass();
 }
+
+bool X86SFIPass::runOnMachineFunction(MachineFunction &MF) {
+    for (MachineBasicBlock &MBB : MF) {
+        LLVM_DEBUG({
+            dbgs() << "Machine Basic Block: ";
+            dbgs() << MBB.getFullName();
+            dbgs() << "\n";
+        });
+    }
+    return true;
+}
+
+
